b9288f90a83e42c84e018eec5dc52cc43b2f562e
Tales-1 <84051594+Tales-1@users.noreply.github.com>
2022-07-20 15:21:34 +0100

    test branch

--- app.js
+++ app.js
@@ -3,36 +3,76 @@ const heroHamzah = document.getElementById("hero-hamzah")
 const gameContainer = document.getElementById("game-container")
 const cardContainer = document.getElementById("card-container")
 const startGame = document.getElementById("start-game")
-
+const characterPage = document.getElementById("first-page")
+let battlePage;
 
 // FLAGS
 
+
 let isGame = false;
-let characterSelected = false;
+let characterInfo = {
+        selected:false,
+        hero:"",
+};
+
 let playerTurn = false;
 let playerMove = false;
-let enemyAlive = false;
+
 
 // EVENT LISTENERS
 
 
 cardContainer.addEventListener("click",(e)=>{
     const targetCard = e.target.closest("div")
+    const heroName = targetCard.children[0].innerHTML
     if(!targetCard || targetCard.id==="card-container") return
-    console.log(targetCard)
-    const cardChildren = Array.from(cardContainer.children)
-    cardChildren.forEach((card)=>{
-        card.classList.remove("selected")
-    })
+    selectCard(heroName)
     targetCard.classList.add("selected")
-    characterSelected = true;
+
+    console.log(characterInfo.hero)
 })
 
-startGame.addEventListener("click",()=>{
 
+startGame.addEventListener("click",()=>{
+    if(characterInfo.selected){
+        isGame = true;
+        characterPage.classList.add("hidden")
+    }
 })
 
 
 
 // FUNCTIONS 
 
+function selectCard(name){
+    const cardChildren = Array.from(cardContainer.children)
+    cardChildren.forEach((card)=>{
+        card.classList.remove("selected")
+    })
+    characterInfo.selected = true;
+    characterInfo.hero = name
+}
+
+{/* <div class="gc__cardcontainer" id="card-container">
+            
+            <div class="gc__card" id="hero-abid">
+                <strong class="card__name gc--white">Bald Abid</strong>
+                <img src="./imgs/bald abid(1).png" alt="Bald abid" class="bald-abid">
+                <!-- <div class="card_moves">
+                    <span class="option">Shiny Headbutt</span>
+                    <span class="option">Shiny Headbutt</span>
+                    <span class="option">Shiny Headbutt</span>
+                    <span class="option">Shiny Headbutt</span>
+                </div> -->
+            </div>
+
+            <div class="gc__card" id="hero-hamzah">
+                <strong class="card__name gc--white">Bald Hamzah</strong>
+                <img src="./imgs/hamzah bald(1).png" alt="Bald abid" class="bald-abid">
+                <!-- <div class="card_moves">
+                    <span class="option">Shiny Headbutt</span>
+                    <span class="option">Shiny Headbutt</span>
+                    <span class="option">Shiny Headbutt</span>
+                    <span class="option">Shiny Headbutt</span>
+                </div> -->
+            </div> */}
